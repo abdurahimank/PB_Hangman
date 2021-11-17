05c81f32ced487d1ff88585c5da79afa6d7a934a
Abdurahiman K <laluvga@gmail.com>
2021-11-17 07:04:33 +0530

    more functions added

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ word_list = ['python', 'java', 'kotlin', 'javascript']
 guess_word = random.choice(word_list)
 hint = "-" * len(guess_word)
 life = 8
+already_guessed = set()
 
 # Main body to check guess word
 while life > 0:
@@ -15,20 +16,27 @@ while life > 0:
     guess_letter = input("Input a letter: ")
     hint = list(hint)
     letters_guess_word = set(guess_word)
-    if guess_letter in letters_guess_word:
-        index = 0
-        if guess_letter not in hint:
-            for spell in guess_word:
-                if spell == guess_letter:
-                    hint[index] = guess_letter
-                index += 1
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+                "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    if len(guess_letter) == 1:
+        if guess_letter in alphabet:
+            if guess_letter in already_guessed:
+                print("You've already guessed this letter")
+            elif guess_letter in letters_guess_word:
+                index = 0
+                already_guessed.add(guess_letter)
+                for spell in guess_word:
+                    if spell == guess_letter:
+                        hint[index] = guess_letter
+                    index += 1
+            else:
+                already_guessed.add(guess_letter)
+                print("That letter doesn't appear in the word")
+                life -= 1
         else:
-            print("No improvements")
-            life -= 1
+            print("Please enter a lowercase English letter")
     else:
-        print("That letter doesn't appear in the word")
-        life -= 1
-
+        print("You should input a single letter")
     hint = ''.join(hint)
     if hint == guess_word:
         break
