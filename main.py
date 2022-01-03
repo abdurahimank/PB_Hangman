60a8c62fd01bee071c4d8bc866ef7dfe6f28ea51
Abdurahiman K <laluvga@gmail.com>
2022-01-03 07:10:54 +0530

    some codes are changed

--- main.py
+++ main.py
@@ -1,58 +1,45 @@
 import random
+import string
 
 
-# Initiation
-print("H A N G M A N")
 while True:
-    status = input('Type "play" to play the game, "exit" to quit:')
-    if status == "play":
-        word_list = ['python', 'java', 'kotlin', 'javascript']
-        guess_word = random.choice(word_list)
-        hint = "-" * len(guess_word)
+    print("H A N G M A N")
+    play = input('Type "play" to play the game, "exit" to quit:')
+    if play == "play":
+        words_list = ['python', 'java', 'kotlin', 'javascript']
+        word = random.choice(words_list)
+        player = "-" * len(word)
         life = 8
-        already_guessed = set()
-
-        # Main body to check guess word
+        guessed_letters = []
         while life > 0:
-            print("")
-            print(hint)
-            guess_letter = input("Input a letter: ")
-            hint = list(hint)
-            letters_guess_word = set(guess_word)
-            alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
-                        "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-            if len(guess_letter) == 1:
-                if guess_letter in alphabet:
-                    if guess_letter in already_guessed:
-                        print("You've already guessed this letter")
-                    elif guess_letter in letters_guess_word:
-                        index = 0
-                        already_guessed.add(guess_letter)
-                        for spell in guess_word:
-                            if spell == guess_letter:
-                                hint[index] = guess_letter
-                            index += 1
-                    else:
-                        already_guessed.add(guess_letter)
-                        print("That letter doesn't appear in the word")
-                        life -= 1
-                else:
-                    print("Please enter a lowercase English letter")
-            else:
+            print()
+            print(player)
+            letter = input("Input a letter: ")
+            if len(letter) != 1:
                 print("You should input a single letter")
-            hint = ''.join(hint)
-            if hint == guess_word:
+                continue
+            if letter not in string.ascii_lowercase:
+                print("Please enter a lowercase English letter")
+                continue
+            if letter in guessed_letters:
+                print("You've already guessed this letter")
+            elif letter in word:
+                player = list(player)
+                idx = 0
+                for i in word:
+                    if i == letter:
+                        player[idx] = letter
+                    idx += 1
+                player = "".join(player)
+            else:
+                print("That letter doesn't appear in the word")
+                life -= 1
+            guessed_letters.append(letter)
+            if player == word:
+                print("You guessed the word!\nYou survived!")
                 break
-
-        # Final output display
-        if hint == guess_word:
-            print("You guessed the word!")
-            print("You survived!")
-            print("")
         else:
             print("You lost!")
-            print("")
-    elif status == "exit":
-        break
-    else:
         continue
+    else:
+        break
