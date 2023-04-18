4be2ab525b75dde1da5d8d159d14b6d4d6278b18
ABDURAHIMAN K <laluvga@gmail.com>
2023-04-18 11:04:29 +0530

    Stage 4/8: Help is on the way

--- stages.py
+++ stages.py
@@ -1,9 +1,9 @@
-# Stage 3/8: Make your choice
+# Stage 4/8: Help is on the way
 import random
 
 
 print("H A N G M A N")
 words = ("python", "java", "swift", "javascript")
 word = random.choice(words)
-guessed_word = input("Guess the word: ")
+guessed_word = input(f"Guess the word {word[:3] + '-' * (len(word) - 3)}")
 print("You survived!" if guessed_word == word else "You lost!")
