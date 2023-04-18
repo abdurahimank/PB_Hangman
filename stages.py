22e318d1bf7fffbb04a04ab6b53add67ec52f561
ABDURAHIMAN K <laluvga@gmail.com>
2023-04-18 12:39:14 +0530

    Stage 5/8: Keep trying

--- stages.py
+++ stages.py
@@ -1,9 +1,24 @@
-# Stage 4/8: Help is on the way
+# Stage 5/8: Keep trying
 import random
 
 
 print("H A N G M A N")
 words = ("python", "java", "swift", "javascript")
 word = random.choice(words)
-guessed_word = input(f"Guess the word {word[:3] + '-' * (len(word) - 3)}")
-print("You survived!" if guessed_word == word else "You lost!")
+attempt = 8
+temp_word = list("-" * len(word))
+while attempt > 0:
+    attempt -= 1
+    print("\n", "".join(temp_word), sep="")
+    letter = input("Input a letter: ")
+    if letter in word:
+        for i in range(len(word)):
+            if letter == word[i]:
+                temp_word[i] = letter
+    else:
+        print("That letter doesn't appear in the word.")
+    # if "".join(temp_word) == word:
+       # break
+# guessed_word = "".join(temp_word)
+# print("You survived!" if guessed_word == word else "You lost!")
+print("Thanks for playing!")
